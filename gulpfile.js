06e6930f34bc4a048f875c61c1c879e5b8b84f2a
Jacob Harris <jacob@outerplaces.com>
2016-07-21 16:46:53 -0400

    version 0.1.0
    barebones version, only the basic grid at this time

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+'use strict';
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var minifyCSS = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+ 
+gulp.task('styles', function() {
+    gulp.src('scss/bicep.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+          browsers: ['last 2 versions'],
+          cascade: false
+        }))
+        //.pipe(minifyCSS({compatibility: 'ie8'}))
+        .pipe(rename('bicep.min.css'))
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('default',function() {
+    gulp.watch('scss/bicep.scss',['styles']);
+});
+
+
